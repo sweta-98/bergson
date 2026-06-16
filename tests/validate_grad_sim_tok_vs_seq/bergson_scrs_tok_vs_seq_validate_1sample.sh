1f27b7a96314959bf060bcf650ef2b5f7341d710
Sweta <swetajena98@gmail.com>
2026-06-16 08:34:56 +0000

    grad sim tests for token scores vs sequence scores

---
+++ tests/validate_grad_sim_tok_vs_seq/bergson_scrs_tok_vs_seq_validate_1sample.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+export CUDA_VISIBLE_DEVICES="0"
+
+# QUERY STEP (animal-query)
+bergson build "./teacher_number_scorings/build_op.part" \
+    --model unsloth/Llama-3.2-1B-Instruct \
+    --dataset "./data/elephant_query_1sample.jsonl" \
+    --prompt_column "prompt" \
+    --completion_column "completion" \
+    --aggregation mean \
+    --projection_dim 16 \
+    --token_batch_size 2048 \
+    --overwrite \
+    --truncation \
+    --filter_modules "*vision*"
+    
+
+# DATASET STEP (teacher data)
+bergson score "./teacher_number_scorings_tok/score" \
+    --model unsloth/Llama-3.2-1B-Instruct \
+    --dataset "./data/elephant_teacher_numbers_1sample.jsonl" \
+    --prompt_column "prompt" \
+    --completion_column "completion" \
+    --query_path "./teacher_number_scorings/build_op.part" \
+    --projection_dim 16 \
+    --token_batch_size 2048 \
+    --overwrite \
+    --truncation \
+    --filter_modules "*vision*"\
+    --attribute_tokens
+
+bergson score "./teacher_number_scorings_seq/score" \
+    --model unsloth/Llama-3.2-1B-Instruct \
+    --dataset "./data/elephant_teacher_numbers_1sample.jsonl" \
+    --prompt_column "prompt" \
+    --completion_column "completion" \
+    --query_path "./teacher_number_scorings/build_op.part" \
+    --projection_dim 16 \
+    --token_batch_size 2048 \
+    --overwrite \
+    --truncation \
+    --filter_modules "*vision*"
+
+python check_tok_sum_vs_seq.py
+            
+
+        
+
+
---
+++ tests/validate_grad_sim_tok_vs_seq/check_tok_sum_vs_seq.py
@@ -0,0 +1,40 @@
+import os
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+# device='cuda'
+
+import sys
+sys.path.insert(0, '../bergson')
+import numpy as np
+from pathlib import Path
+from bergson.data import load_scores
+
+seq_scr_path = "./teacher_number_scorings_seq/score"
+scr_path = "./teacher_number_scorings_tok/score"
+
+seq_scrs = load_scores(Path(seq_scr_path))
+seq_scrs = np.array([score[0] for score in seq_scrs])
+
+offsets = np.load(scr_path+'/offsets.npy')
+num_token_grads=np.load(scr_path+'/num_token_grads.npy')
+total_tokens = int(offsets[-1])
+scores = np.memmap(
+    Path(scr_path) / "token_scores.bin",
+    dtype=np.float32,
+    mode="r",
+    shape=(total_tokens,),
+)
+
+for i in range(1): # len(dataset)
+    ex_scores = scores[offsets[i] : offsets[i + 1]]
+    print("---------------")
+    print("Sequence score:", seq_scrs[i])
+    print("Token scores sum:", ex_scores.sum())
+    print("Token scores mean:", ex_scores.mean())
+    print("---------------")
+    if ex_scores.sum() == seq_scrs[i]:
+        print("---> TEST PASSED")
+    else:
+        print("---> TEST FAILED")
