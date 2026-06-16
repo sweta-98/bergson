261576faddb7d0b7a747ab66b99097edc066befa
Sweta <swetajena98@gmail.com>
2026-06-16 20:05:35 +0000

    Don't save the query artifacts in .part

--- tests/validate_grad_sim_tok_vs_seq/bergson_scrs_tok_vs_seq_validate_1sample.sh
+++ tests/validate_grad_sim_tok_vs_seq/bergson_scrs_tok_vs_seq_validate_1sample.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 export CUDA_VISIBLE_DEVICES="0"
 
 # QUERY STEP (animal-query)
-bergson build "./teacher_number_scorings/build_op.part" \
+bergson build "./teacher_number_scorings/build_op" \
     --model unsloth/Llama-3.2-1B-Instruct \
     --dataset "./data/elephant_query_1sample.jsonl" \
     --prompt_column "prompt" \
@@ -24,7 +24,7 @@ bergson score "./teacher_number_scorings_tok/score" \
     --dataset "./data/elephant_teacher_numbers_1sample.jsonl" \
     --prompt_column "prompt" \
     --completion_column "completion" \
-    --query_path "./teacher_number_scorings/build_op.part" \
+    --query_path "./teacher_number_scorings/build_op" \
     --projection_dim 16 \
     --token_batch_size 2048 \
     --overwrite \
@@ -37,7 +37,7 @@ bergson score "./teacher_number_scorings_seq/score" \
     --dataset "./data/elephant_teacher_numbers_1sample.jsonl" \
     --prompt_column "prompt" \
     --completion_column "completion" \
-    --query_path "./teacher_number_scorings/build_op.part" \
+    --query_path "./teacher_number_scorings/build_op" \
     --projection_dim 16 \
     --token_batch_size 2048 \
     --overwrite \
